1bb86f439a2b812e94c3dc960e92593f3fb03071
russormes <russormes@gmail.com>
2014-02-18 12:25:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyA2p::Application.config.secret_key_base = '8cf613bd29f8a5c375eeea30537e2118b68d765cd5c1ff09043f16f522ca542bd80c8f740e8d8dc03444e3caa82fdce217ec19e3e50cb0cf420e6c37167a429a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyA2p::Application.config.session_store :cookie_store, key: '_myA2p_session'
